c91c0cbeb99083c7532e01099d7d278259a05a76
Ravi Ranjan <ravi848101mnb@gmail.com>
2024-01-04 21:07:29 +0530

    cors

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
 const express = require("express");
 const mongoose = require("mongoose");
+const cors = require("cors");
 require("dotenv").config();
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.use(express.json());
+app.use(cors());
 
 const areaRoutes = require("./routes/route.areas.js");
 app.use("/api", areaRoutes);
@@ -13,6 +15,16 @@ app.use("/api", areaRoutes);
 mongoose.connect(process.env.MONGODB_URI, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
+});
+
+const db = mongoose.connection;
+
+db.on("error", (error) => {
+  console.error("MongoDB connection error:", error);
+});
+
+db.once("open", () => {
+  console.log("Connected to MongoDB database");
 });
 
 app.listen(PORT, () => {
