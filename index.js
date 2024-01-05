cf4a687c763062ccc1c23a248f4b8dfb9f9d0f22
Ravi Ranjan <ravi848101mnb@gmail.com>
2024-01-05 12:15:18 +0530

    cors_disabbled

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
 const express = require("express");
 const mongoose = require("mongoose");
-const cors = require("cors");
+// const cors = require("cors");
 require("dotenv").config();
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.use(express.json());
-app.use(cors()); 
+// app.use(cors()); 
 
 const areaRoutes = require("./routes/route.areas.js");
 app.use("/api", areaRoutes);
